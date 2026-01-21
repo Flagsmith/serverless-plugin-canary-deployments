6ad6bd13fc786360f87983c2caca6fd43f5cc757
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-01-21 19:32:21 +0100

    handle breaking changes in constructor / imports

--- serverless-plugin-canary-deployments.test.js
+++ serverless-plugin-canary-deployments.test.js
@@ -2,15 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const chai = require('chai')
 const _ = require('lodash/fp')
-const { getInstalledPathSync } = require('get-installed-path')
 const ServerlessCanaryDeployments = require('./serverless-plugin-canary-deployments')
+const Serverless = require('serverless')
 
-const serverlessPath = getInstalledPathSync('serverless', { local: true })
-const Serverless = require(`${serverlessPath}/lib/Serverless`)
-const serverlessVersion = parseInt((new Serverless()).version)
-const AwsProvider = serverlessVersion > 1
-  ? require(`${serverlessPath}/lib/plugins/aws/provider`)
-  : require(`${serverlessPath}/lib/plugins/aws/provider/awsProvider`)
 const { expect } = chai
 const fixturesPath = path.resolve(__dirname, 'fixtures')
 
@@ -36,11 +30,24 @@ describe('ServerlessCanaryDeployments', () => {
     )
 
     this.testCases.forEach(({ caseName, input, output, service }) => {
-      it(`generates the correct CloudFormation templates: test case ${caseName}`, () => {
-        const serverless = new Serverless(options)
+      it(`generates the correct CloudFormation templates: test case ${caseName}`, async () => {
+        const serverless = new Serverless({
+          configuration: {
+            service: 'canary-deployments-test',
+            provider: { name: 'aws' },
+            configValidationMode: 'off',
+            plugins: []
+          },
+          commands: [],
+          options
+        })
+
+        // Initialize serverless (this automatically sets up the AWS provider)
+        await serverless.init()
+
         Object.assign(serverless.service, service)
         serverless.service.provider.compiledCloudFormationTemplate = input
-        serverless.setProvider('aws', new AwsProvider(serverless, options))
+
         const plugin = new ServerlessCanaryDeployments(serverless, options)
         plugin.addCanaryDeploymentResources()
         expect(serverless.service.provider.compiledCloudFormationTemplate).to.deep.equal(output)
